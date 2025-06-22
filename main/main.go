16440b376761336720ae646fb9c88f55675d565f
AmarnathSahu <amarnathsahu78@gmail.com>
2025-06-22 14:56:13 +0530

    Program One

--- main/main.go
+++ main/main.go
@@ -1,9 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func main() {
-	input := "AABBBDDDD"
+func Program_To_Get_Occurence_Character(input string) string {
 	MapData := make(map[string]int)
 	for _, ch := range input {
 		str := string(ch)
@@ -19,5 +20,20 @@ func main() {
 	for key, value := range MapData {
 		result += fmt.Sprintf("%s%d", key, value)
 	}
+	return result
+}
+
+func Swap_Elemnts_Of_List(list []int, i, j int) []int {
+	list[i], list[j] = list[j], list[i]
+	return list
+}
+
+func main() {
+	input := "AABBBDDDD"
+	result := Program_To_Get_Occurence_Character(input)
 	fmt.Println(result)
+
+	list := []int{1, 2, 3, 4, 5}
+	list = Swap_Elemnts_Of_List(list, 0, 1)
+	fmt.Println(list)
 }
